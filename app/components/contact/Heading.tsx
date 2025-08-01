adfdae06851028e3382a3a34fe48fd89bf9ed475
Sherif Sani <sanisherif11@gmail.com>
2025-08-01 17:27:47 +0100

    Add Heading component for contact page with introductory text

---
+++ app/components/contact/Heading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div>
+        <h2 className="text-xl font-bold text-white">Get in Touch</h2>
+          <p>
+              Let's connect and start something great,<br />
+              or we could discuss an idea!
+        </p>
+    </div>
+  )
+}
+
+export default Heading
