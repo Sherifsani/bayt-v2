c45a4a6bf3b0aeb1547708c7a1a9bad525be3e0a
Sherif Sani <sanisherif11@gmail.com>
2025-08-01 18:17:36 +0100

    Refactor Heading component: enhance layout with flexbox and improved styling

--- app/components/contact/Heading.tsx
+++ app/components/contact/Heading.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Heading = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-2'>
         <h2 className="text-xl font-bold text-white">Get in Touch</h2>
-          <p>
+          <p className='text-gray-300 '>
               Let's connect and start something great,<br />
               or we could discuss an idea!
         </p>
